5912a0b2501efd4fd78717b5dcddc6d65a4af929
Devansh <devansh.jagtap@cdgi.edu.in>
2026-03-14 18:05:37 +0530

    auth is working with the help of nextauth and the google as the provider added in future we will add the personal jwt and all login .signup and the prisma working as the ORM and neon using for the headless server on the postgrelSQL

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,20 @@
-export default function Dashboard() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return <p>Please login</p>;
+  }
   return (
     <div className="p-10  min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
 
-      <h1 className="text-3xl font-bold mb-6">
-        Dashboard
-      </h1>
-
-       <p className="text-xl font-bold text-gray-700">Welcome to your dashboard</p>
+      <p className="text-xl font-bold text-gray-700">
+        Welcome to your dashboard  {session.user?.name}
+      </p>
       <div className="grid grid-cols-3 mt-10 gap-10 text-center">
-
         <div className="bg-white  p-10 text-3xl rounded-xl shadow">
           Total Invoices
         </div>
@@ -17,12 +23,8 @@ export default function Dashboard() {
           Subscription
         </div>
 
-        <div className="bg-white p-10  text-3xl rounded-xl shadow">
-          Revenue
-        </div>
-
+        <div className="bg-white p-10  text-3xl rounded-xl shadow">Revenue</div>
       </div>
-
     </div>
-  )
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,21 @@
-export default function LoginPage(){
+"use client"
 
-  return(
+import { signIn } from "next-auth/react"
+
+export default function LoginPage() {
+  return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-xl shadow-md w-96">
         <h1 className="text-2xl text-center font-bold mb-6">
           Quid-Invoice Generator
         </h1>
-        <button className="bg-black text-white text-lg rounded-lg w-50 py-1 mx-15 border">
+        <button
+          onClick={() => signIn("google")}
+          className="bg-black text-white text-lg rounded-lg w-50 py-1 mx-15 border"
+        >
           Sign in with google
         </button>
       </div>
     </div>
   );
-
 }
